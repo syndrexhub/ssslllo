aa5e586a61c5055323dab17d0973a0687ed46615
SynDrex Hub <52568232+syndrexhub@users.noreply.github.com>
2021-09-13 18:08:20 +0800

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -10,67 +10,78 @@ fi
 red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
-if [ -f "/etc/v2ray/domain" ]; then
-echo 
-exit 0
+MYIP=$(wget -qO- ifconfig.co);
+echo "Checking VPS"
+IZIN=$( curl https://github.com/syndrexhub/AutoScript-SSH-VPN/raw/main/ipvps | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${green}Permission Accepted...${NC}"
+else
+echo -e "${green}Permission Accepted...${NC}"
 fi
 mkdir /var/lib/premium-script;
 echo "IP=" >> /var/lib/premium-script/ipvps.conf
-wget https://raw.githubusercontent.com/slaungther/0/main/cf.sh && chmod +x cf.sh && ./cf.sh
+wget https://raw.githubusercontent.com/racunzx/script_kiddie/main/cf.sh && chmod +x cf.sh && ./cf.sh
 #install ssh ovpn
-wget https://raw.githubusercontent.com/slaungther/0/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
-wget https://raw.githubusercontent.com/slaungther/0/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
+wget https://raw.githubusercontent.com/racunzx/script_kiddie/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && screen -S ssh-vpn ./ssh-vpn.sh
+wget https://raw.githubusercontent.com/racunzx/script_kiddie/main/sstp.sh && chmod +x sstp.sh && screen -S sstp ./sstp.sh
 #install ssr
-wget https://raw.githubusercontent.com/slaungther/0/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
-wget https://raw.githubusercontent.com/slaungther/0/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
-#installwg
-wget https://raw.githubusercontent.com/slaungther/0/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
+wget https://raw.githubusercontent.com/racunzx/script_kiddie/main/ssr.sh && chmod +x ssr.sh && screen -S ssr ./ssr.sh
+wget https://raw.githubusercontent.com/racunzx/script_kiddie/main/sodosok.sh && chmod +x sodosok.sh && screen -S ss ./sodosok.sh
+#install wireguard
+wget https://raw.githubusercontent.com/racunzx/script_kiddie/main/wg.sh && chmod +x wg.sh && screen -S wg ./wg.sh
 #install v2ray
-wget http://raw.githubusercontent.com/slaungther/0/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
+wget https://raw.githubusercontent.com/racunzx/script_kiddie/main/ins-vt.sh && chmod +x ins-vt.sh && screen -S v2ray ./ins-vt.sh
 #install L2TP
-wget https://raw.githubusercontent.com/slaungther/0/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
-wget https://raw.githubusercontent.com/slaungther/0/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
-
+wget https://raw.githubusercontent.com/racunzx/script_kiddie/main/ipsec.sh && chmod +x ipsec.sh && screen -S ipsec ./ipsec.sh
+wget https://raw.githubusercontent.com/racunzx/script_kiddie/main/set-br.sh && chmod +x set-br.sh && ./set-br.sh
+#install trojan-go
+wget https://raw.githubusercontent.com/racunzx/script_kiddie/main/ins-trojango.sh && dos2unix ins-trojango.sh && chmod +x ins-trojango.sh && ./ins-trojango.sh
+#log
+wget https://raw.githubusercontent.com/racunzx/script_kiddie/main/log.sh && dos2unix log.sh && chmod +x log.sh && ./log.sh
 rm -f /root/ssh-vpn.sh
 rm -f /root/sstp.sh
-rm -f /root/wg.sh
 rm -f /root/ss.sh
 rm -f /root/ssr.sh
+rm -f /root/wg.sh
 rm -f /root/ins-vt.sh
 rm -f /root/ipsec.sh
 rm -f /root/set-br.sh
+rm -f /root/ins-trojango.sh
+rm -f /root/log.sh
+
 cat <<EOF> /etc/systemd/system/autosett.service
 [Unit]
 Description=autosetting
-Documentation=https://vpnstores.net
-
+Documentation=https://premium-server.xyz
 [Service]
 Type=oneshot
 ExecStart=/bin/bash /etc/set.sh
 RemainAfterExit=yes
-
 [Install]
 WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
 systemctl enable autosett
-wget -O /etc/set.sh "https://raw.githubusercontent.com/slaungther/0/main/set.sh"
-chmod +x /etc/set.sh
+#wget -O /etc/set.sh "https://raw.githubusercontent.com/agian123/gasken/main/set.sh"
+#chmod +x /etc/set.sh
 history -c
 echo "1.2" > /home/ver
 clear
 echo " "
 echo "Installation has been completed!!"
 echo " "
-echo "=================================-Autoscript Premium-===========================" | tee -a log-install.txt
+echo "===============================-{ Autoscript Premium }-=========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
 echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 442"  | tee -a log-install.txt
-echo "   - Stunnel4                : 443, 777"  | tee -a log-install.txt
+echo "   - Stunnel4                : 902, 777"  | tee -a log-install.txt
 echo "   - Dropbear                : 109, 143"  | tee -a log-install.txt
+echo "   - WebSocket/WS SSL        : 443"  | tee -a log-install.txt
+echo "   - WebSocket/WS Non SSL    : 2082"  | tee -a log-install.txt
+echo "   - WebSocket/WS Ovpn       : 2086"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080 (limit to IP Server)"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                   : 81"  | tee -a log-install.txt
@@ -86,6 +97,7 @@ echo "   - V2RAY Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - V2RAY Vless TLS         : 2083"  | tee -a log-install.txt
 echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
 echo "   - Trojan                  : 2087"  | tee -a log-install.txt
+echo "   - TrojanGO                : 2096"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
 echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
@@ -93,7 +105,7 @@ echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
 echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
 echo "   - IPtables                : [ON]"  | tee -a log-install.txt
 echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
-echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]" | tee -a log-install.txt
 echo "   - Autoreboot On 05.00 GMT +7" | tee -a log-install.txt
 echo "   - Autobackup Data" | tee -a log-install.txt
 echo "   - Restore Data" | tee -a log-install.txt
@@ -102,15 +114,8 @@ echo "   - Full Orders For Various Services" | tee -a log-install.txt
 echo "   - White Label" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "   - Dev/Main                : Rh"  | tee -a log-install.txt
-echo "   - Telegram                : T.me/malu"  | tee -a log-install.txt
-echo "   - Instagram               : @Rh"  | tee -a log-install.txt
-echo "   - Whatsapp                : 0819473349"  | tee -a log-install.txt
-echo "   - Facebook                : https://www.facebook.com/Rh" | tee -a log-install.txt
-echo "------------------Script Created By Rh-----------------" | tee -a log-install.txt
-echo ""
-echo " Reboot 15 Sec"
-sleep 15
+echo "==========================-[ AUTOSCRIPT PREMIUM ]-===========================" | tee -a log-install.txt
+echo " Reboot 10 Sec"
+sleep 10
 rm -f setup.sh
 reboot
-
