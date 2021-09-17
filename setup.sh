2f84aceee89fb6380f5bcc163d6dd1564ecbfc7c
SynDrex Hub <52568232+syndrexhub@users.noreply.github.com>
2021-09-17 21:47:31 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,15 +11,7 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 MYIP=$(wget -qO- icanhazip.com);
-IZIN=$( curl https://raw.githubusercontent.com/Daorakle/Daorakle/main/ipvps | grep $MYIP )
-if [ $MYIP = $IZIN ]; then
-echo -e "${green}Permission Accepted...${NC}"
-else
-echo -e "${red}Permission Denied!${NC}";
-echo "Please Contact Admin"
-rm -f setup.sh
-exit 0
-fi
+clear
 if [ -f "/etc/v2ray/domain" ]; then
 echo "Script Already Installed"
 exit 0
